18f4f8080d9197831a0ba13104a856a4d53ffc2b
toriqnaufilnadzar <tnaufilnadzar@gmail.com>
2021-10-26 11:31:57 +0700

    Tugas 2 : base admin template

---
+++ resources/views/index2.blade.php
@@ -0,0 +1,8 @@
+@extends('layout/main')
+@section('content')
+<div class="container">
+    <div class="content-wrapper">
+        <h5>ini index 2</h5>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('2', function () {
+    return view('index2');
 });
